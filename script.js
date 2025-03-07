b151e6fd667e0bb11e6d42490d521e2aead46e08
nathan-au <im.nathanau@gmail.com>
2025-03-06 22:00:10 -0500

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function toggleDarkMode() {
+    document.querySelectorAll(".portfolio-container").forEach(
+        element => {
+            element.classList.toggle("dark-mode");
+        }
+    );
     document.body.classList.toggle("dark-mode");
     // document.getElementById("github-logo").classList.toggle("invert-colors");
     document.querySelectorAll(".invertible-logo").forEach(
