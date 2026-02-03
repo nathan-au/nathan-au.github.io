dcf7548951ed8eef2739d1dee40b142d76b8b828
nathan-au <im.nathanau@gmail.com>
2026-02-02 22:06:52 -0500

    resume link and tech stacks

--- projects.js
+++ projects.js
@@ -3,7 +3,7 @@ const projects = [
         name: "FabricMatch API",
         image_path: "./assets/screens/fabricmatchapi_screen.jpg",
         description: "Computer vision API that extracts body and facial data for personalized style recommendations.",
-        tech_stack: ["Python, FastAPI, YOLO, DeepFace, OpenCV, Hugging Face Transformers, Gradio Client"],
+        tech_stack: ["Python", "FastAPI", "Ultralytics YOLO", "DeepFace", "OpenCV", "Hugging Face Transformers", "Gradio Client"],
         modal_id: "fabricmatchapi_modal",
         git_repo: "https://github.com/nathan-au/fabric-match-api"   
     },
@@ -11,7 +11,7 @@ const projects = [
         name: "Scaffold",
         image_path: "./assets/screens/scaffold_screen.png",
         description: "Reward-based learning reinforcement platform that transforms kids' screen time into learning time.",
-        tech_stack: ["Next.js, TypeScript, Supabase, Tailwind CSS, Vercel, ElevenLabs API, Gemini API"],
+        tech_stack: ["Next.js", "TypeScript", "Supabase", "Tailwind CSS", "Vercel", "ElevenLabs API", "Gemini API"],
         modal_id: "scaffold_modal",
         git_repo: "https://github.com/nathan-au/scaffold"   
     },
@@ -26,7 +26,7 @@ const projects = [
     {
         name: "Instagram Opps v3",
         image_path: "./assets/screens/instagramoppsv3_screen.png",
-        description: "Script for detecting your Instagram opps (people you follow that don't follow you back).",
+        description: "Script for detecting your Instagram opps (people don't follow you back).",
         tech_stack: ["Python", "Playwright"],
         modal_id: "instagramoppsv3_modal",
         git_repo: "https://github.com/nathan-au/instagram-opps-v3"
@@ -59,7 +59,7 @@ const projects = [
         name: "PartCart",
         image_path: "./assets/screens/partcart_screen.png",
         description: "Internal communication platform that simplifies the part procurement process for computer repair operations.",
-        tech_stack: ["Vue.js", "Supabase", "TailwindCSS"],
+        tech_stack: ["Vue.js", "Supabase", "Tailwind CSS"],
         modal_id: "partcart_modal",
         git_repo: "https://github.com/nathan-au/part-cart"
     },
@@ -75,7 +75,7 @@ const projects = [
         name: "West Carleton Grad System",
         image_path: "./assets/screens/gradd_screen.png",
         description: "Full-stack web app for creating and managing slideshows for graduation ceremonies.",
-        tech_stack: ["HTML", "CSS", "Bootstrap", "JavaScript", "PHP", "SQL", "MySQL", "Google OAuth API", "jQuery", "TinyMCE"],
+        tech_stack: ["HTML", "CSS", "Bootstrap", "JavaScript", "PHP", "SQL", "Google OAuth API", "TinyMCE"],
         modal_id: "gradd_modal",
         git_repo: "https://github.com/nathan-au/grad-d"
     }
@@ -89,7 +89,6 @@ projects.forEach(project => {
     let imageHTML = "<figure><img src=\"" + project.image_path + "\" alt=\"" + project.name + "\" /></figure>"
 
     let techStackHTML = ""
-    project.tech_stack.sort()
     project.tech_stack.forEach(tech => {
         techStackHTML += "<span class=\"badge badge-soft\">" + tech + "</span>"
     })
@@ -102,8 +101,11 @@ projects.forEach(project => {
         <div class="card-body">
             <h2 class="card-title">${project.name}</h2>
             <p class="opacity-75">${project.description}</p>
-            <div class="card-actions mt-5 justify-end">
-                <a href="${project.git_repo}" target="_blank" class="btn bg-black text-white border-black">
+            <div class="flex flex-row flex-wrap gap-2 mt-2">
+                ${techStackHTML}
+            </div>
+            <div class="card-actions mt-2 justify-end">
+                <a href="${project.git_repo}" target="_blank" class="btn bg-black text-white border-black hover:scale-105 transition-transform duration-200">
                     <svg aria-label="GitHub logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="white" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"></path></svg>
                     ${project.name} on GitHub
                 </a>
